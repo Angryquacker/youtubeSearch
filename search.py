6accc42c50755bcee04034056ef3b74802f00b7f
Travis Hahn <tjxhahn@gmail.com>
2018-11-19 15:18:20 -0500

    Fixed Deletion
    File is now deleted after the song is complete, or if the user exits the program early

--- search.py
+++ search.py
@@ -1,8 +1,10 @@
 from __future__ import unicode_literals
 import requests
 import youtube_dl
+import os
 from bs4 import BeautifulSoup
 from playsound import playsound
+from random import randint
 
 #Get Keyword
 query = input("Enter Query: ")
@@ -18,6 +20,15 @@ for a in source_code.findAll('a'):
         video = 'https://www.youtube.com' + video
         break
 
+#create a random string as the name of the new file as the prevent replaying self/wrong song
+def gen_random_name():
+    global name
+    string = []
+    s = ''
+    for i in range(10):
+        string.append(chr(randint(65, 90)))
+    name = s.join(string)
+    return name
 
 #Have the youtube-dl download only the video's audio track
 ydl_opts = {
@@ -27,7 +38,7 @@ ydl_opts = {
         'preferredcodec': 'mp3',
         'preferredquality': '192',
     }],
-    "outtmpl": './audio.m4a'
+    "outtmpl": './' + gen_random_name() + '.m4a'
 }
 
 
@@ -36,9 +47,14 @@ with youtube_dl.YoutubeDL(ydl_opts) as ydl:
     ydl.download([video])
 
 
-#play the audio
-playsound('./audio.mp3')
+#Try/Catch block ensures video is deleted even if program is exited early
+try:
+    #play the audio
+    playsound('./' + name +'.mp3')
+except:
+    # Delete the file when done
+    os.remove('./' + name + '.mp3')
 
 
-
-
+#Delete the file when done
+os.remove('./' + name + '.mp3')
